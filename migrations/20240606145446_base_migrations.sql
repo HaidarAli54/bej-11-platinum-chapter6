1da0c1b94583f8ae07b1b97c699d0eba6e51b064
HaidarAli54 <haidarali5464@gmail.com>
2024-06-06 08:07:14 -0700

    migrates

--- migrations/20240606145446_base_migrations.sql
+++ migrations/20240606145446_base_migrations.sql
@@ -1,5 +1,24 @@
 -- migrate:up
+table users {
+  id int [not null]
+  username varchar(255) [not null]
+  email varchar(100) [not null]
+  password varchar(100) [not null]
+  updated_at timestamp [default: null]
+  created_at timestamp 
+  deleted_at timestamp [default: null]
+}
 
+table post {
+  id int [not null]
+  userid int [not null]
+  foto image [not null]
+  description varchar
+  likes varchar
+}
+
+Ref: "users"."id" < "post"."userid"
 
 -- migrate:down
-
+DROP TABLE IF EXISTS "users";
+DROP TABLE IF EXISTS "post";
