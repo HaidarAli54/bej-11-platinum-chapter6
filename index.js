f3ec5f649e69909d5079ed1a20c499f2ef535905
HaidarAli54 <haidarali5464@gmail.com>
2024-06-06 06:56:39 -0700

    index

--- index.js
+++ index.js
@@ -3,15 +3,12 @@ const app = express();
 
 require('dotenv').config();
 
-// Mendapatkan port dari environment variable atau default ke 3000
 const port = process.env.PORT || 3000;
 
-// Mendefinisikan route untuk endpoint /ping
 app.get('/ping', (req, res) => {
-    res.status(200).json({ message: 'PONG!!' }); // Mengirim respons JSON dengan pesan 'PONG!!'
+    res.status(200).json({ message: 'PONG!!' }); 
 });
 
-// Menjalankan server pada port yang telah ditentukan
 app.listen(port, () => {
-    console.log(`Server berjalan pada port ${port}`); // Menampilkan pesan di console bahwa server berjalan
+    console.log(`Server berjalan pada port ${port}`); 
 });
